95e9ac0aebb3cac7b4c4e4d2bb2e531fde0348c6
Roxana <roxanaradutza@gmail.com>
2023-08-11 12:22:29 +0200

    añadir index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+export const sum = (a, b) => {
+    return a + b;
+  };
+  
+  export const rest = (a, b) => {
+    return a - b;
+  };
+  
+  export const multiply = (a, b) => {
+    return a * b;
+  };
+  
+  export const div = (a, b) => {
+    return a / b;
+  };
+  
+  export const mod = (a, n) => {
+    return a % n;
+  };
